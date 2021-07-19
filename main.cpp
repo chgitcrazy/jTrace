9d996e9f30cf8230084d4f94c95ddf2a1bc029b9
ch <chawxw@gmail.com>
2021-07-19 22:39:30 +0800

    remove unused files

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@
 
 using namespace std;
 
+
 JNIEXPORT jint JNICALL
-Agent_OnAttach(JavaVM* vm, char* options, void* reserved){
+Agent_OnLoad(JavaVM *vm, char *options, void *reserved){
     jvmtiEnv *jvmti;
     jint result = vm->GetEnv((void**)&jvmti,JVMTI_VERSION_1_1);
     if (result != JNI_OK){
@@ -31,18 +32,50 @@ Agent_OnAttach(JavaVM* vm, char* options, void* reserved){
     err = jvmti->GetThreadState(NULL,&thread_state);
 
 
-//    for (int i = 0; i < 100 ; i++){
-//        char *sig;
-//        jvmti->GetClassSignature(classes[i],&sig,NULL);
-//        printf("i=%d ,class sig=%s\n",i,sig);
-//    }
-//
-//    printf("error:%d\n",err);
+    for (int i = 0; i < 100 ; i++){
+        char *sig;
+        jvmti->GetClassSignature(classes[i],&sig,NULL);
+        printf("i=%d ,class sig=%s\n",i,sig);
+    }
 
 
-
+    printf("error:%d\n",err);
     return err;
 
-
 }
+
+
+//JNIEXPORT jint JNICALL
+//Agent_OnAttach(JavaVM* vm, char* options, void* reserved){
+//    jvmtiEnv *jvmti;
+//    jint result = vm->GetEnv((void**)&jvmti,JVMTI_VERSION_1_1);
+//    if (result != JNI_OK){
+//        printf("ERROR: Unable to access JVMTI!\n");
+//    }
+//
+//    jvmtiError err = (jvmtiError)0;
+//    jclass *classes;
+//    jint count;
+//    err = jvmti->GetLoadedClasses(&count,&classes);
+//    if (err){
+//        printf("ERROR: JVMIT GetLoadedClasses failed!\n");
+//    }
+//
+//    jint thread_state;
+//    err = jvmti->GetThreadState(NULL,&thread_state);
+//
+//
+////    for (int i = 0; i < 100 ; i++){
+////        char *sig;
+////        jvmti->GetClassSignature(classes[i],&sig,NULL);
+////        printf("i=%d ,class sig=%s\n",i,sig);
+////    }
+////
+////    printf("error:%d\n",err);
+//
+//
+//
+//    return err;
+//
+//}
 
